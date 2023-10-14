a674310951cbe87269bc3dee3d58dc2220e2476c
illocarlos <carlosalbendiz93@gmail.com>
2023-10-14 21:56:20 +0200

    change to tailwing

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,43 +1,53 @@
-import { Button, Container } from 'react-bootstrap'
-import './HomePage.css'
+
 
 
 const HomePage = () => {
     return (
 
-        <div className='HomePage'>
+        <div className="h-screen w-screen flex flex-col flex-nowrap justify-around bg-gradient-to-tr from-gray-950 to-gray-700">
 
-            <div className='firstBlock'>
-                <div className='home-Page-Intro'>
+            <div className="flex flex-row flex-nowrap justify-around">
+                <div className="w-2/5 ml-10">
 
-                    <div className='home-h1'>
+                    <div className="text-8xl bg-gradient-to-tr from-green-500  to-blue-400 text-transparent bg-clip-text no-underline">
                         <h1>Welcome to HappyCod</h1>
                     </div>
-                    <div>
+                    <p className="text-white mt-16 text-2xl">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    </div>
+                    </p>
 
                 </div>
+                <div className=" w-2/6 h-3/4 flex flex-col flex-nowrap items-center justify-around ">
+                    <article className=" items-center text-white text-lg ">
 
-                <article className='Home-Article'>
-                    <Container>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
                         Dolore eius explicabo ipsum molestiae suscipit voluptas
                         optiolibero! Veritatis fugiat sapiente vitae odit magni
                         eveniet, optio, eaque dolore placeat ut quod?
 
-                    </Container>
-                    <Button className='Home-Article-Button'>lorem ipsum</Button>
-                </article>
 
+                    </article>
+                    <button class=" w-2/4 bg-gradient-to-tr from-green-400 to-blue-400 rounded-3xl py-1 px-2 text-black  ">
+                        Lorem ipsum
+                    </button>
+                </div>
             </div>
 
             <div>
-                <div>proyect</div>
-                <div>About us</div>
-                <div>Service</div>
+                <div className="w-2/5 h-20 bg-gradient-to-r from-blue-800 to-transparent text-white" >
+                    <h3>proyect</h3>
+                    <button></button>
+                </div>
+                <div className=" w-2/5 h-20 bg-gradient-to-r from-blue-400 to-transparent text-white">
+                    <h3>About us</h3>
+                    <button></button>
+                </div>
+                <div className=" w-2/5 h-20 bg-gradient-to-r from-green-400 to-transparent text-black">
+                    <h3> Service</h3>
+                    <button></button>
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
 export default HomePage
