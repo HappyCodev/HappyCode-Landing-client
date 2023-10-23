a3fd1e795da2f872b37c768b7824a26d714b44fc
nacho <nachosanson6@gmail.com>
2023-10-23 13:55:11 +0200

    modal in progress

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,36 @@
-
+import { useState } from 'react';
+import logo from './../../assets/images/LOGO 2.png'
+import { Link } from 'react-router-dom';
+import Contact from '../Contact/Contact';
 
 
 const Navigation = () => {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <div className='Navigation' >
-            <h2>Navigation</h2>
+            <Link to={'/'}>
+                <img src={logo} alt="HappyCode Logo" />
+            </Link>
+            <button onClick={openModal}>Contact Us</button>
+
+            {isModalOpen && (
+                <div className='modal'>
+                    <div className='modal-content'>
+                        <span className='close' onClick={closeModal}>&times;</span>
+                        <Contact />
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
