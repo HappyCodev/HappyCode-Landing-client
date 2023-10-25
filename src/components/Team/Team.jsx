652306406b36c8b3873ea069d49dd40bde6c9f0c
Joan Benaiges <jobeto87@gmail.com>
2023-10-25 20:31:42 +0100

    doing team

--- src/components/Team/Team.jsx
+++ src/components/Team/Team.jsx
@@ -1,24 +1,115 @@
-
-
 const Team = () => {
+
     return (
+
         <div className='Team' >
+
             <h1>Meet our team</h1>
+
             <div className="membersRow">
 
-                <div className="member">
-                    <img className="memeberImage" src="" alt="" />
+                <div className="memberCard">
+                    <div className="alejandraMamblonaIMG" />
                     <p className="memberName">Alejandra Mamblona</p>
-                    <p className="memeberOccupation">UX/UI Designer</p>
-                    <p className="memberContact">LinkedIn Email</p>
+                    <p className="memberOccupation">UX/UI Designer</p>
+                    <p className="memberContact">
+                        <a className="linkedInLogo" href="#"></a>
+                        <a className="emailLogo" href="#"></a>
+                    </p>
+                </div>
+
+                <div className="memberCard">
+                    <div className="isabellaPaivaIMG" />
+                    <p className="memberName">Isabella Paiva</p>
+                    <p className="memberOccupation">Product Manager</p>
+                    <p className="memberContact">
+                        <a className="linkedInLogo" href="#"></a>
+                        <a className="emailLogo" href="#"></a>
+                    </p>
+                </div>
+
+                <div className="memberCard">
+                    <div className="ignacioSansonIMG" />
+                    <p className="memberName">Ignacio Sansón</p>
+                    <p className="memberOccupation">Lead Full Stack</p>
+                    <p className="memberContact">
+                        <a className="linkedInLogo" href="#"></a>
+                        <a className="emailLogo" href="#"></a>
+                    </p>
+                </div>
+
+                <div className="memberCard">
+                    <div className="carlosDelgadoIMG" />
+                    <p className="memberName">Carlos Delgado</p>
+                    <p className="memberOccupation">Front-end Manager</p>
+                    <p className="memberContact">
+                        <a className="linkedInLogo" href="#"></a>
+                        <a className="emailLogo" href="#"></a>
+                    </p>
                 </div>
 
             </div>
+
             <div className="membersRow">
+
+                <div className="memberCard">
+                    <div className="alejandroRecheIMG" />
+                    <p className="memberName">Alejandro Reche</p>
+                    <p className="memberOccupation">Back-end developer</p>
+                    <p className="memberContact">
+                        <a className="linkedInLogo" href="#"></a>
+                        <a className="emailLogo" href="#"></a>
+                    </p>
+                </div>
+
+                <div className="memberCard">
+                    <div className="carlosAlbendizIMG" />
+                    <p className="memberName">Carlos Albéndiz</p>
+                    <p className="memberOccupation">Front-end developer</p>
+                    <p className="memberContact">
+                        <a className="linkedInLogo" href="#"></a>
+                        <a className="emailLogo" href="#"></a>
+                    </p>
+                </div>
+
+                <div className="memberCard">
+                    <div className="ireneBucetaIMG" />
+                    <p className="memberName">Irene Buceta</p>
+                    <p className="memberOccupation">Front-end developer</p>
+                    <p className="memberContact">
+                        <a className="linkedInLogo" href="#"></a>
+                        <a className="emailLogo" href="#"></a>
+                    </p>
+                </div>
+
+                <div className="memberCard">
+                    <div className="joanBenaigesIMG" />
+                    <p className="memberName">Joan Benaiges</p>
+                    <p className="memberOccupation">Front-end developer</p>
+                    <p className="memberContact">
+                        <a className="linkedInLogo" href="https://www.linkedin.com/in/joan-benaiges-94bb9460/"></a>
+                        <a className="emailLogo" href="#"></a>
+                    </p>
+                </div>
+
+                <div className="memberCard">
+                    <div className="rodrigoLopezIMG" />
+                    <p className="memberName">Rodrigo López</p>
+                    <p className="memberOccupation">Back-end developer</p>
+                    <p className="memberContact">
+                        <a className="linkedInLogo" href="#"></a>
+                        <a className="emailLogo" href="#"></a>
+                    </p>
+                </div>
+
+
 
 
             </div>
+
         </div>
+
     )
 }
+
 export default Team
